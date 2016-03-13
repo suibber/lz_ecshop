3c672a7943f4d3f8368ff3ceeb9b0edd2fda2658
suibber <suibber@gmail.com>
2016-03-13 16:21:18 +0800

    add | sql

--- temp/compiled/help.lbi.php
+++ temp/compiled/help.lbi.php
@@ -4,10 +4,10 @@
 ?>
 <dl>
   <dt><a href='<?php echo $this->_var['help_cat']['cat_id']; ?>' title="<?php echo $this->_var['help_cat']['cat_name']; ?>"><?php echo $this->_var['help_cat']['cat_name']; ?></a></dt>
-  <?php $_from = $this->_var['help_cat']['article']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'item_0_99030000_1457629740');if (count($_from)):
-    foreach ($_from AS $this->_var['item_0_99030000_1457629740']):
+  <?php $_from = $this->_var['help_cat']['article']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'item_0_42295000_1457807104');if (count($_from)):
+    foreach ($_from AS $this->_var['item_0_42295000_1457807104']):
 ?>
-  <dd><a href="<?php echo $this->_var['item_0_99030000_1457629740']['url']; ?>" title="<?php echo htmlspecialchars($this->_var['item_0_99030000_1457629740']['title']); ?>"><?php echo $this->_var['item_0_99030000_1457629740']['short_title']; ?></a></dd>
+  <dd><a href="<?php echo $this->_var['item_0_42295000_1457807104']['url']; ?>" title="<?php echo htmlspecialchars($this->_var['item_0_42295000_1457807104']['title']); ?>"><?php echo $this->_var['item_0_42295000_1457807104']['short_title']; ?></a></dd>
   <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>
 </dl>
 <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>
